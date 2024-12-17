2b7b8e50b1807daf57a2b2f7510f16a0b5ab49b3
Sukh Singh <sukh@orbs.com>
2024-12-17 15:43:39 +0000

    fix example create button

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -67,7 +67,7 @@ function App() {
                   setError(err as Error);
                 }
               }}
-              disabled={!signerName || !signerExpiryMs}
+              disabled={!signerName}
             >
               Create
             </button>
